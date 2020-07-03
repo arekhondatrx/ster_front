3d7c70ba88b7cfe5abed9aa53ab0a108238184ae
Arek <hallmann92@wp.pl>
2020-07-03 14:48:45 +0200

    vue app

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="appka">
+    <h1> Tabela sygnalizatorów </h1>
+    <traffic-state :signaler="signaler"/>
+  </div>
+</template>
+
+<script>
+import trafficState from './components/trafficState.vue';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const wsClient = new W3CWebSocket('ws://localhost:2000');
+
+export default {
+    name: 'App',
+    components: {
+        trafficState
+    },
+    data: () => {
+        return {
+        signaler: [],
+        connectionStatus: 'Not connected to backend server.'
+        }
+    },
+    methods: {
+
+    },
+    created() {
+        wsClient.onopen = () => {
+          // this.signaler = 'connected...';
+        };
+        wsClient.onmessage = (msg) => {
+          this.signaler = JSON.parse(msg.data);
+        };
+        wsClient.onclose = () => {
+          // this.signaler = 'conection close...';
+        }
+
+    }
+}
+</script>
+
+<style>
+.appka {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    background-color: #e9efec;
+    float:left;
+    width: 100%;
+}
+</style>
---
+++ src/components/trafficState.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="ts">
+    <table>
+      <thead>
+      <tr>
+        <th>HOST URL</th>
+        <th>ID</th>
+        <th>STATE</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="row in signaler">
+        <td>{{row.url}}</td>
+        <td>{{row.id}}</td>
+        <td id="state">
+            <div>
+              {{row.state}}
+              <canvas width="20" height="20" v-bind:style="{
+                                                            'background-color': row.state,
+                                                             marginLeft: 3 + 'px',
+                                                             marginTop: 5 + 'px'
+                                                             }"></canvas>
+            </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+    <p></p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'trafficState',
+  props: {
+      signaler: {
+        type: Object,
+        required: true
+      }
+  }
+}
+</script>
+
+<style>
+  #ts {
+      background-color: inherit;
+  }
+
+  #state {
+      width: 160px;
+      text-align: center;
+  }
+
+  table {
+      font-family: 'Open Sans', sans-serif;
+      width: auto;
+      margin: 10px auto;
+      border-collapse: collapse;
+      border: 3px solid #44475C;
+      font-size: 25px;
+      background-color: #e6f7ef;
+  }
+
+  table th {
+    text-align: center;
+    background: #44475C;
+    color: #FFF;
+    padding: 8px;
+    min-width: 30px;
+  }
+
+  table td {
+    text-align: center;
+    padding: 8px;
+    border-right: 2px solid #7D82A8;
+  }
+
+  table tr {
+    border-bottom: 2px solid #7D82A8;
+  }
+  table td:last-child {
+    border-right: none;
+  }
+  /*table tbody tr:nth-child(2n) td {*/
+  /*  background: #D4D8F9;*/
+  /*}*/
+</style>
